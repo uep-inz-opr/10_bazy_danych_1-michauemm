c331d84b5b79c7023d8943942d8ea2113e2f1610
michauemm <102590836+michauemm@users.noreply.github.com>
2022-06-09 17:41:07 +0200

    poprawka 2

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import csv, sqlite3
 if __name__ == "__main__":
-    sqlite_con = sqlite3.connect(":memory:", detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
+    con = sqlite3.connect(":memory:", detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
     cur = sqlite_con.cursor()
     cur.execute('''CREATE TABLE polaczenia (from_subscriber data_type INTEGER, 
                     to_subscriber data_type INTEGER, 
